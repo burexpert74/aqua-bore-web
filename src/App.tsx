10c071ebf5023b01fd2f12c8035e016a661fab7b
Wh0mail <124348737+Wh0mail@users.noreply.github.com>
2025-05-30 23:03:02 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Blog from "./pages/Blog";
-import BlogPost from "./pages/BlogPost";
+import Blog from "./pages/blog"; // ✅ Папка с index.tsx
+import BlogPost from "./pages/blog/[slug]"; // ✅ корректно
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
