02aeb4f4b53c9d09f5bbb03754f432321c1ac377
Wh0mail <124348737+Wh0mail@users.noreply.github.com>
2025-06-02 16:04:40 +0500

    Create genmap.js

---
+++ src/components/genmap.js
@@ -0,0 +1,26 @@
+import { slugs } from '../../src/components/getBlogPosts.tsx';
+import fs from 'fs';
+import path from 'path';
+
+
+// Пути
+const sitemapPath = path.resolve(__dirname, '../../public/sitemap.xml');
+const baseUrl = 'https://www.burexert.com';
+
+const urls = slugs.map(slug => `
+  <url>
+    <loc>${baseUrl}/blog/${slug}</loc>
+  </url>
+`).join('');
+
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+>
+${urls}
+</urlset>
+`;
+
+fs.writeFileSync(sitemapPath, sitemap.trim());
+
+console.log('✅ sitemap.xml создан!');
